b420d9ebc6aa3825673709ffda4a8fb1b18b1e41
Yong72 <2009110026@office.kopo.ac.kr>
2022-09-30 15:11:26 +0900

    함수교체, 사이클 수정

--- temsor_test/tensor_test.py
+++ temsor_test/tensor_test.py
@@ -9,16 +9,21 @@ ydata = data['admit'].values
 xdata = []
 for i, rows in data.iterrows():
     xdata.append([rows['gre'], rows['gpa'], rows['rank']])
+#gre: 영어 성적
+#gpa: 학점
+#rank: 대학 순위(낮을 수록 좋음)
+#admit: 합격 여부(0: 불합격, 1: 합격)
+
 #신경망 구축
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(32, activation='elu', input_shape=(3,)),
+    tf.keras.layers.Dense(32, activation='elu'),
     tf.keras.layers.Dense(64, activation='leaky_relu'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
 #학습
 model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(np.array(xdata), np.array(ydata), epochs=500)
+model.fit(np.array(xdata), np.array(ydata), epochs=250)
 
 #추론
-예측값 = model.predict(np.array([[750, 3.70, 3], [800, 4.0, 1]]))
-print(예측값)
+inference = model.predict(np.array([[750, 3.70, 3], [800, 4.0, 1], [100, 2.0, 1], [600, 2.0, 4], [800, 3.0, 2]]))
+print(inference)
