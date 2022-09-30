8164a2e8eb4f000d85f51f2f97737d5d9d37aed7
Yong72 <2009110026@office.kopo.ac.kr>
2022-09-30 14:15:56 +0900

    주석 추가

--- temsor_test/tensor_test.py
+++ temsor_test/tensor_test.py
@@ -1,26 +1,22 @@
-
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-
+#데이터 입력 및 전처리
 data = pd.read_csv('gpascore.csv')
 data = data.dropna()
-
+#데이터 분할 및 정규화
 ydata = data['admit'].values
 xdata = []
 for i, rows in data.iterrows():
     xdata.append([rows['gre'], rows['gpa'], rows['rank']])
-
-
-
+#신경망 구축
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(128, activation='tanh'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
-
+#학습
 model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
-
 model.fit(np.array(xdata), np.array(ydata), epochs=500)
 
 #추론
